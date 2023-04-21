0b2e43a1216e7108469d687aaf6d2e9ca3b91646
dreiadotwav <ria.schittner@gmail.com>
2023-04-21 17:04:43 -0400

    a

--- other/themes_properties.route.js
+++
@@ -1,8 +0,0 @@
-const themes_propertiesController = require('../../controller/themes_properties/themes_properties.controller');
-
-module.exports = function(app) {
-
-    app.get("/themesp/list", themes_propertiesController.listar);
-    app.get("/themesp/update", themes_propertiesController.actualizar);
-    app.delete("/themesp/delete/:id", themes_propertiesController.eliminar);
-}
